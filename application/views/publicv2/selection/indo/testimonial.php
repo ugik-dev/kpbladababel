1dc23a920d4351871b8b60460f96f20124393d11
ugik <ugik.dev@gmail.com>
2021-03-03 15:28:06 +0700

    ss

--- application/views/publicv2/selection/indo/testimonial.php
+++ application/views/publicv2/selection/indo/testimonial.php
@@ -4,12 +4,10 @@
         <div class="row d-flex justify-content-center">
             <div class="col-lg-8">
                 <div class="testimonial-text text-center">
-                    <h5 class="testimonial-title">Happy Clients</h5>
-                    <h2 class="testimonial-subtitle">Testimonial of Clients</h2>
+                    <h5 class="testimonial-title"></h5>
+                    <h2 class="testimonial-subtitle">Our Teams</h2>
                     <p class="testimonial-title-describe">
-                        We have many happy investors invest with us .Some impresions
-                        from our Customers! PLease read some of the lovely things our
-                        Customers say about us.
+                    We have any professional team with us .Some impressions from our Teams! PLease read some of the lovely things our Teams say about us.
                     </p>
                 </div>
             </div>
@@ -23,95 +21,50 @@
                                 “Great service! I have been worried about investing. But
                                 when I came here. I don't have to worry anymore’’
                             </p>
-                            <h2 class="client-name">Joy Kelley</h2>
-                            <h4 class="client-date">United kingdom, 28th April,2019</h4>
+                            <h2 class="client-name">Prof. Drs. H. Saparudin, M.T.</h2>
+                            <!-- <h4 class="client-date">United kingdom, 28th April,2019</h4> -->
                         </div>
                         <div class="carousel-img">
                             <div class="img-outline">
-                                <img src="img/table-img2.png" alt="#" />
+                                <img src="<?=base_url('assets/')?>img/saparudin.jpg" alt="#" />
+                                <!-- <img src="img/table-img2.png" alt="#" /> -->
                             </div>
                         </div>
                     </div>
-
                     <div class="carousel-item">
                         <div class="carousel-caption">
                             <p class="client-describe">
                                 “Great service! I have been worried about investing. But
                                 when I came here. I don't have to worry anymore’’
                             </p>
-                            <h2 class="client-name">Joy Kelley</h2>
-                            <h4 class="client-date">United kingdom, 28th April,2019</h4>
+                            <h2 class="client-name">Sugi Pramana, S.Kom</h2>
+                            <h4 class="client-date">IT Support</h4>
                         </div>
                         <div class="carousel-img">
                             <div class="img-outline">
-                                <img src="img/table-img2.png" alt="#" />
+                                <!-- <img src="img/table-img2.png" alt="#" /> -->
+                                <img src="<?=base_url('assets/')?>img/sugi-pramana.jpg" alt="#" />
+
                             </div>
                         </div>
                     </div>
-
+      
                     <div class="carousel-item">
                         <div class="carousel-caption">
                             <p class="client-describe">
                                 “Great service! I have been worried about investing. But
                                 when I came here. I don't have to worry anymore’’
                             </p>
-                            <h2 class="client-name">Joy Kelley</h2>
-                            <h4 class="client-date">United kingdom, 28th April,2019</h4>
+                            <h2 class="client-name">Deki Susanto, S.T</h2>
+                            <h4 class="client-date">General Manager</h4>
                         </div>
                         <div class="carousel-img">
                             <div class="img-outline">
-                                <img src="img/table-img2.png" alt="#" />
+                                <img src="<?=base_url('assets/')?>img/deki-susanto.jpg" alt="#" />
                             </div>
                         </div>
                     </div>
-
-                    <div class="carousel-item">
-                        <div class="carousel-caption">
-                            <p class="client-describe">
-                                “Great service! I have been worried about investing. But
-                                when I came here. I don't have to worry anymore’’
-                            </p>
-                            <h2 class="client-name">Joy Kelley</h2>
-                            <h4 class="client-date">United kingdom, 28th April,2019</h4>
-                        </div>
-                        <div class="carousel-img">
-                            <div class="img-outline">
-                                <img src="img/table-img2.png" alt="#" />
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="carousel-item">
-                        <div class="carousel-caption">
-                            <p class="client-describe">
-                                “Great service! I have been worried about investing. But
-                                when I came here. I don't have to worry anymore’’
-                            </p>
-                            <h2 class="client-name">Joy Kelley</h2>
-                            <h4 class="client-date">United kingdom, 28th April,2019</h4>
-                        </div>
-                        <div class="carousel-img">
-                            <div class="img-outline">
-                                <img src="img/table-img2.png" alt="#" />
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="carousel-item">
-                        <div class="carousel-caption">
-                            <p class="client-describe">
-                                “Great service! I have been worried about investing. But
-                                when I came here. I don't have to worry anymore’’
-                            </p>
-                            <h2 class="client-name">Joy Kelley</h2>
-                            <h4 class="client-date">United kingdom, 28th April,2019</h4>
-                        </div>
-                        <div class="carousel-img">
-                            <div class="img-outline">
-                                <img src="img/table-img2.png" alt="#" />
-                            </div>
-                        </div>
-                    </div>
+      
                 </div>
             </div>
         </div>
